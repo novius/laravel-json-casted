6e37f10493b0b7975e75d1e73f1cc576e36fc58c
Gilles Felix <felix@novius.fr>
2024-12-11 12:13:20 +0100

    Improve JsonCastable

--- src/Casts/JsonCastable.php
+++ src/Casts/JsonCastable.php
@@ -8,7 +8,7 @@ use Novius\LaravelJsonCasted\Classes\JsonCasted;
 
 class JsonCastable implements CastsAttributes
 {
-    public function __construct(protected string $getCastsMethod = 'getCasts') {}
+    public function __construct(protected mixed $casts = []) {}
 
     /**
      * @throws JsonException
@@ -20,14 +20,18 @@ class JsonCastable implements CastsAttributes
         }
 
         $value = json_decode($value, true, 512, JSON_THROW_ON_ERROR);
-        if (method_exists($model, $this->getCastsMethod)) {
-            $cast = $model->{$this->getCastsMethod}();
+        if (method_exists($model, $this->casts)) {
+            $cast = $model->{$this->casts}();
 
             return new JsonCasted($value, $cast);
         }
 
-        if (is_subclass_of($this->getCastsMethod, JsonCasted::class)) {
-            return new $this->getCastsMethod($value);
+        if (is_subclass_of($this->casts, JsonCasted::class)) {
+            return new $this->casts($value);
+        }
+
+        if (is_array($this->casts)) {
+            return new JsonCasted($value, $this->casts);
         }
 
         return new JsonCasted($value);
